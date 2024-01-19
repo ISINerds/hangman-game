aad976914e3bd59fd97f8c569b861b23c14c3515
amine-a11 <mohamedaminehajji11@yahoo.com>
2024-01-19 18:34:40 +0100

    graphviz integration

--- src/includes/data-structures/binary_tree.h
+++ src/includes/data-structures/binary_tree.h
@@ -4,12 +4,14 @@
 typedef struct BinaryTree BinaryTree;
 struct BinaryTree{
     char data;
+    int id;
     BinaryTree* left_child;
     BinaryTree* right_child;
 };
+int counter=0;
 // ------------------------------functions signitures------------------------
 BinaryTree* addWord(BinaryTree* root,const char* word);
-// void printBinaryTree(BinaryTree* root,int* cc);
+void printBinaryTree(BinaryTree* root,int* cc);
 
 // -------------------------------end functions signitures---------------------
 
@@ -18,17 +20,19 @@ BinaryTree* addWord(BinaryTree* root,const char* word);
 
 // --------------------------------------------hajji--------------------------
 
-BinaryTree* addWord(BinaryTree* root,const char* word){
+BinaryTree* addWordTest(BinaryTree* root,const char* word){
     if(root==NULL){
         if(word[0]!='\0'){
             root=(BinaryTree*)malloc(sizeof(BinaryTree));
             root->data=word[0];
+            root->id=counter++;
             root->right_child=NULL;
             root->left_child=addWord(root->left_child,word+1);
             return root;
         }else{
             root=(BinaryTree*)malloc(sizeof(BinaryTree));
             root->data=word[0];
+            root->id=counter++;
             root->right_child=NULL;
             root->left_child=NULL;
             return root;
@@ -43,24 +47,82 @@ BinaryTree* addWord(BinaryTree* root,const char* word){
         }
     }
 }
-/*
-This function will assist us in displaying the tree.
-The only challenge associated with it is the necessity to assign
-a unique identifier to each node.
-This is crucial because there might be nodes with identical values,
-making it difficult to visualize the structure accurately.
-*/
-// void printBinaryTree(BinaryTree* root,int* cc){
-//     if(root->left_child!=NULL)printf("\"%c-%d\" -> \"%c-%d\"\n",root->data,root->id,root->left_child->data,root->left_child->id);
-//     else{
-//         printf("null%d [shape=point]\n",*cc);
-//         printf("\"%c-%d\" -> \"null%d\"\n",root->data,root->id,(*cc)++);
-//     }
-//     if(root->right_child!=NULL)printf("\"%c-%d\" -> \"%c-%d\"\n",root->data,root->id,root->right_child->data,root->right_child->id);
-//     else{
-//         printf("null%d [shape=point]\n",*cc);
-//         printf("\"%c-%d\" -> \"null%d\"\n",root->data,root->id,(*cc)++);
-//     }
-//     if(root->left_child!=NULL)printBinaryTree(root->left_child,cc);
-//     if(root->right_child!=NULL)printBinaryTree(root->right_child,cc);
+void printBinaryTreeToFile(FILE* file, BinaryTree* root,int* cc){
+    if(root->left_child!=NULL){
+        if(root->data=='\0'){
+            fprintf(file, "Node%d [label=\"\\\\0\"]\n",root->id);
+        }else{
+            fprintf(file, "Node%d [label=\"%c\"]\n",root->id,root->data);
+        }
+        if(root->left_child->data=='\0'){
+            fprintf(file, "Node%d [label=\"\\\\0\"]\n",root->left_child->id);
+        }else{
+            fprintf(file, "Node%d [label=\"%c\"]\n",root->left_child->id,root->left_child->data);
+        }
+        fprintf(file, "\"Node%d\" -> \"Node%d\"\n",root->id,root->left_child->id);
+    }
+    else{
+        fprintf(file, "null%d [shape=point]\n",*cc);
+        if(root->data=='\0'){
+            fprintf(file, "Node%d [label=\"\\\\0\"]\n",root->id);
+        }else{
+            fprintf(file, "Node%d [label=\"%c\"]\n",root->id,root->data);
+        }
+        fprintf(file, "\"Node%d\" -> \"null%d\"\n",root->id,(*cc)++);
+    }
+    if(root->right_child!=NULL){
+        if(root->data=='\0'){
+            fprintf(file, "Node%d [label=\"\\\\0\"]\n",root->id);
+        }else{
+            fprintf(file, "Node%d [label=\"%c\"]\n",root->id,root->data);
+        }
+        if(root->right_child->data == '\0'){
+            fprintf(file, "Node%d [label=\"\\\\0\"]\n",root->right_child->id);
+        }else{
+            fprintf(file, "Node%d [label=\"%c\"]\n",root->right_child->id,root->right_child->data);
+        }
+        fprintf(file, "\"Node%d\" -> \"Node%d\"\n",root->id,root->right_child->id);
+    }
+    else{
+        fprintf(file, "null%d [shape=point]\n",*cc);
+        if(root->data=='\0'){
+            fprintf(file, "Node%d [label=\"\\\\0\"]\n",root->id);
+        }else{
+            fprintf(file, "Node%d [label=\"%c\"]\n",root->id,root->data);
+        }
+
+        fprintf(file, "\"Node%d\" -> \"null%d\"\n",root->id,(*cc)++);
+    }
+    if(root->left_child!=NULL)printBinaryTreeToFile(file,root->left_child,cc);
+    if(root->right_child!=NULL)printBinaryTreeToFile(file,root->right_child,cc);
+}
+void generateImageFromBinaryTree(BinaryTree* root){
+    FILE *file = fopen("tree.dot", "w");
+    if (file == NULL) {
+        fprintf(stderr, "Error opening file.\n");
+        return;
+    }
+    int cc=0;
+    fprintf(file,"digraph G {\n");
+    printBinaryTreeToFile(file, root, &cc);
+    fprintf(file,"}");
+    fclose(file);
+    int result = system("dot tree.dot | gvpr -c -ftree.gv | neato -n -Tsvg -o trie.svg");
+    if (result == -1) {
+        perror("Error executing command");
+    }
+}
+// #include<stdio.h>
+// #include "./includes/data-structures/binary_tree.h"
+// int main(void){
+//     BinaryTree* root=NULL;
+//     root=addWord(root,"cas");
+//     root=addWord(root,"ce");
+//     root=addWord(root,"ces");
+//     root=addWord(root,"ci");
+//     root=addWord(root,"de");
+//     root=addWord(root,"des");
+//     root=addWord(root,"do");
+//     generateImageFromBinaryTree(root);
+//     return 0;
 // }
