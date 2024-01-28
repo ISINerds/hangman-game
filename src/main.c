e1f928d9cdb9209ba747a5f1a86bd8c6936c36d0
amine-a11 <mohamedaminehajji11@yahoo.com>
2024-01-28 19:57:09 +0100

    adding raylib example

--- src/main.c
+++ src/main.c
@@ -2,47 +2,36 @@
 #include "./includes/utils/file_parsing.h"
 #include "./includes/data-structures/binary_tree.h"
 #include "./includes/data-structures/trie.h"
+#include "raylib.h"
 int main(void){
-    Words words = parseFile("./assets/words.txt");
-    // char *words[] = {"cas", "ce", "ces", "ci", "de", "des", "do"};
-    const int size_Words = words.words_array_size;
-    ListTrie dict = initiliazeListTrie(26);
+    WordList words = parseFile("./assets/words.txt");
+    BinaryTree* root = NULL;
+    root = addWords(words,root);
+    generateImageFromBinaryTree(root,"./build/tree.svg","./assets/tree.gv");
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    for (int j=0; j<size_Words; j++) {
-        char firstSymbol = words.words_array[j][0];
-        if (dict.array_roots[getAlphabeticalOrder(firstSymbol) -1 ] == NULL ) {
-            Trie* trieP = initiliazeTrie(firstSymbol);
-            if (trieP!=NULL) {
-                dict.array_roots[getAlphabeticalOrder(firstSymbol)-1]= trieP;
-            }else {
-                printf("Error in allocation of TrieP pointer \n");
-            }
-        }
-        addWordToTrie(words.words_array[j],1,strlen(words.words_array[j]), dict.array_roots[getAlphabeticalOrder(firstSymbol)-1]);
-        
+    // Initialize window
+    
+    InitWindow(screenWidth, screenHeight, "Simple Raylib Example");
+
+    // Main game loop
+    while (!WindowShouldClose()) {
+        // Update
+        // (This example doesn't need updates)
+
+        // Draw
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE); // Clear background color
+
+        // Draw a rectangle
+        DrawRectangle(screenWidth/4, screenHeight/4, screenWidth/2, screenHeight/2, MAROON);
+
+        EndDrawing();
     }
 
-    // for (int i=0; i< 26; i++) {
-    //     if (dict.array_roots[i]!=NULL){
-    //         printf("Array at : %d has value : %c \n",getAlphabeticalOrder(dict.array_roots[i]->data) - 1,dict.array_roots[i]->data);
-    //         if(i<25) {
-    //             printf("the node with value %c has a right sibling : %c \n",dict.array_roots[i]->data, getFirstRightLeftChild(i+1,1,dict.array_roots)->data);
-    //         }
-    //     }
-    // }
-    // char* str = "ccc";
-    // printf("The word : %s, exists? : %d \n",str,wordExists(str,0,strlen(str),dict.array_roots[getAlphabeticalOrder(str[0])-1 ] ) );
-
-    BinaryTree* BT =convertTrieListToBT(dict.array_roots);
-    Queue* q = (Queue*)malloc(sizeof(Queue));
-    q->front = q->rear = NULL;
-    printf("business = %d\n",search_word("businesss",BT,q));
-    generateImageFromBinaryTree(BT,"cool_tree");
-    //recursiveBFS(BT);
-    free(BT);
-    free(dict.array_roots);
-    // BinaryTree* root = addWords(wordsroot);
-
-
+    // Clean up
+    CloseWindow();
     return 0;
 }
