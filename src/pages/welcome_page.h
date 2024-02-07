37c71200faf1dd0efec88e8d1827e564b3ce3058
Saber Mahjoub <saber.mahjoub003@gmail.com>
2024-02-07 19:12:40 +0100

    Fixing responsivness of the welcome page

--- src/pages/welcome_page.h
+++ src/pages/welcome_page.h
@@ -6,6 +6,17 @@ void updateWelcomePage(GameState* state,int screen_width,int screen_height);
 
 bool start_button_clicked = false;
 
+Color black = (Color){ 0,0,0, 255 };
+Color gray = (Color){ 81,105,162, 255 };
+Color dark_blue = (Color){ 26,31,68, 0 };
+Color green = (Color){0,178,13, 255};
+Color purple = (Color){154,52,161,255};
+Color yellow = (Color){255,216,0,255};
+Color pink = (Color){232,0,145,255};
+Color blue = (Color){0,156,225,255};
+Color orange = (Color){255,141,0,255};
+
+
 void drawWelcomePage(GameState* state,int screen_width,int screen_height){
     float roundness = 1.0f;
     float width = 500.0f;
@@ -13,19 +24,14 @@ void drawWelcomePage(GameState* state,int screen_width,int screen_height){
     float segments = 0.0f;
     float line_thick = 3.0f;
 
-
+    int w = GetRenderWidth();
+    int h = GetRenderHeight();
     // Calculate the position of the rectangle to be in the middle of the window
-    Rectangle rec = {(float)(screen_width - width) / 2, (float)(screen_height - height) / 2, width, height};
+    // Rectangle rec = {(float)(screen_width - width) / 2, (float)(screen_height - height) / 2, width, height};
+    Rectangle rec = {w/7, h/4, 0.7*w, 0.3*h};
 
-    Color black = (Color){ 0,0,0, 255 };
-    Color gray = (Color){ 81,105,162, 255 };
-    Color dark_blue = (Color){ 26,31,68, 0 };
-    Color green = (Color){0,178,13, 255};
-    Color purple = (Color){154,52,161,255};
-    Color yellow = (Color){255,216,0,255};
-    Color pink = (Color){232,0,145,255};
-    Color blue = (Color){0,156,225,255};
-    Color orange = (Color){255,141,0,255};
+    Color circle_colors[] = { green, yellow, orange, pink, blue, purple };
+
 
     DrawLine(560, 0, 560, screen_height, Fade(black, 0.6f));
     DrawRectangle(560, 0, screen_width - 500, screen_height, Fade(black, 0.3f));
@@ -37,26 +43,34 @@ void drawWelcomePage(GameState* state,int screen_width,int screen_height){
     const char* text = "HANGMAN GAME";
     Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 30, 1);
 
-    float textX = rec.x + (rec.width - text_size.x) / 2;
-    float textY = rec.y + (rec.height - text_size.y) / 2;
+    //float textX = rec.x + (rec.width - text_size.x) / 2;
+    float textX = rec.x + w/5;
+    // float textY = rec.y + (rec.height - text_size.y) / 2;
+    float textY = rec.y + h/8;
 
-    DrawText(text, textX, textY, 30, WHITE);
+    DrawText(text, textX, textY, h/14, WHITE);
 
     //circles
-    float circle_radius = 10.0f;
-    float centerX = rec.x + rec.width / 2;
-    float centerY = rec.y + rec.height / 2;
+    float circle_radius = h/50;
+    //float centerX = rec.x + rec.width / 2;
+    float centerX = textX + w/5.5;
+    //float centerY = rec.y + rec.height / 2;
+    float centerY = rec.y + h/15;
+
     float total_width = 6 * (circle_radius * 2 + 5); // Total width of all circles and gaps
 
     float circlesY = textY - 30.0f; 
-    Color circle_colors[] = { green, yellow, orange, pink, blue, purple };
 
     for (int i = 0; i < 6; ++i) {
+        // float circleX = centerX - total_width / 2 + i * (circle_radius * 2 + 5);
+        // DrawCircle(circleX, circlesY, circle_radius, circle_colors[i]);
         float circleX = centerX - total_width / 2 + i * (circle_radius * 2 + 5);
-        DrawCircle(circleX, circlesY, circle_radius, circle_colors[i]);
+        DrawCircle(circleX, centerY, circle_radius, circle_colors[i]);
     }
 
-    Rectangle button_rec = { centerX - 100 / 2, rec.y + rec.height + 10, 100, 50 };
+    // Rectangle button_rec = { centerX - 100 / 2, rec.y + rec.height + 10, 100, 50 };
+    Rectangle button_rec = { rec.x + w/3.2, rec.y + h/3 + 10, 100, 50 };
+
     // Check if the mouse button is pressed within the button boundaries
     if (CheckCollisionPointRec(GetMousePosition(), button_rec) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
         start_button_clicked = true;
@@ -65,7 +79,7 @@ void drawWelcomePage(GameState* state,int screen_width,int screen_height){
     }
 
     DrawRectangleRec(button_rec, dark_blue);
-    DrawText("START", button_rec.x + 10, button_rec.y + 7, 20, yellow);
+    DrawText("START !", button_rec.x, button_rec.y + 7, h/18, yellow);
 }
 
 void updateWelcomePage(GameState* state,int screen_width,int screen_height){
